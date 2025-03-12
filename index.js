f884566b5fd2b02b138f5bd18888e0bec322e3a2
Mati Joseph <crucialniccur@gmail.com>
2025-03-12 20:35:46 +0300

    define titleCased function and try accesing the inner values

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+// titleCased = tutorials.map((item) => {
+//   return item.toUpperCase;
+// });
+// console.log(titleCased);
+function titleCased() {
+  return tutorials.map((robot) => {
+    return robot + " trial";
+  });
 }
+
+console.log(titleCased());
